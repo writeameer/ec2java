7c2c9a762231c40d09821a9f691ee5fd55354d2b
Ameer Deen <writeameer@gmail.com>
2012-02-29 19:10:14 +1100

    first commit

---
+++ src/EC2Test.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.*;
+import com.amazonaws.auth.*;
+import java.util.Map;
+
+public class EC2Test {
+
+	static private AmazonEC2Client ec2Client;
+	static private AWSCredentials awsCredentials;
+	static Map<String, String> env = System.getenv();
+	static private String awsAccessKeyId = env.get("AWS_ACCESS_KEY_ID");
+	static private String awsSecretAccessKey  = env.get("AWS_SECRET_ACCESS_KEY");
+	
+	public EC2Test(){
+		 awsCredentials = new BasicAWSCredentials(awsAccessKeyId,awsSecretAccessKey);
+		 ec2Client = new AmazonEC2Client(awsCredentials);
+	}
+
+	@Test
+	public void DescribeInstances() {
+		DescribeInstancesRequest request = new DescribeInstancesRequest();
+ 
+		DescribeInstancesResult response = ec2Client.describeInstances(request);
+		System.out.println(response.toString());
+			
+	}
+}
